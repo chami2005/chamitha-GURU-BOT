2992ecf5d89e98bf8f66cd127fc164eaf8922c3c
chami2005 <165267102+chami2005@users.noreply.github.com>
2024-03-28 03:54:08 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94741022376', 'me', true],
+  ['94741022376', 'chamitha', true], 
   [''] 
 ] //Number of owners
 
